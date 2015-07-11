30482026017ebcb0aeb8cf316dfce95f7dfbc277
yangsj <yangsj@yangsj-PC>
2015-07-11 12:49:47 +0800

    修改库结构

---
+++ begincode/begincode-core/src/main/java/org/core/model/BaseModel.java
@@ -0,0 +1,8 @@
+package org.core.model;
+
+import java.util.Date;
+
+public class BaseModel {
+    private Integer beginRowNum;
+    private Integer limitNum;
+}
---
+++ begincode/begincode-doc/src/main/rar/ddl/1.0.0/alter-begincode_code-1.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `begincode_code`
+	DROP COLUMN `bc_navigation_id`;
