1a48da8b5c54a53b51fb36ad046cfb347de518c2
liu913306275 <913306275@qq.com>
2015-07-30 23:16:03 +0800

    tijiao

---
+++ begincode/begincode-course/src/test/java/org/begincode/course/service/BegincodeCourseServiceTest.java
@@ -0,0 +1,34 @@
+/**   
+ * @Title: BegincodeCourseServiceTest.java 
+ * @Package org.begincode.course.service 
+ * @Description: TODO 
+ * @author liutao   
+ * @date 2015年7月30日 下午11:04:55 
+ * @version V1.0   
+ */
+package org.begincode.course.service;
+
+import javax.annotation.Resource;
+
+import org.begincode.core.model.BegincodeCourse;
+import org.begincode.course.contant.Constants;
+import org.begincode.course.context.BaseTest;
+
+/**
+ * @ClassName: BegincodeCourseServiceTest
+ * @Description: TODO
+ * @author liutao
+ * @date 2015年7月30日 下午11:04:55
+ * 
+ */
+public class BegincodeCourseServiceTest extends BaseTest {
+
+	@Resource
+	private BegincodeCourseService begincodeCourseService;
+
+	public void testCreate() {
+		BegincodeCourse begincodeCourse = new BegincodeCourse(null, "java 课程第一节", "http://img.mukewang.com/558ba3980001db0e06000338-240-135.jpg", "jianjie",
+				"1", 5, 234, 1, 1, "大牛", "1", 1, Constants.DELETE_FLAG_NOMAL, "video");
+		begincodeCourseService.create(begincodeCourse);
+	}
+}
