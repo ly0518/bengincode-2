6d068f828f8c39f4a6f2b0eb79965c54f9c592cd
liu913306275 <913306275@qq.com>
2015-07-31 18:09:37 +0800

    提交

---
+++ begincode/begincode-core/src/main/java/org/begincode/core/contant/Contants.java
@@ -0,0 +1,26 @@
+/**   
+ * @Title: BGContants.java 
+ * @Package org.begincode.core.contant 
+ * @Description: begincode静态常量 
+ * @author liutao   
+ * @date 2015年7月31日 下午6:00:26 
+ * @version V1.0   
+ */
+package org.begincode.core.contant;
+
+/**
+ * @ClassName: Contants
+ * @Description: begincode静态常量
+ * @author liutao
+ * @date 2015年7月31日 下午6:00:26
+ * 
+ */
+public final class Contants {
+
+	/**
+	 * 删除标识 delete_flag, 1：可用 0：删除
+	 */
+	public final static String DELETE_FLAG_YES = "0";
+	public final static String DELETE_FLAG_NOMAL = "1";
+
+}
