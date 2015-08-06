d30d321ea4a564faf093747c8be602ef3cf78971
yangsj <yangsj@yangsj-PC>
2015-08-07 07:44:40 +0800

    常亮命名错误修改

--- begincode/begincode-core/src/main/java/org/begincode/core/constant/BeginCodeConstant.java
+++ begincode/begincode-core/src/main/java/org/begincode/core/constant/BeginCodeConstant.java
@@ -6,16 +6,16 @@
  * @date 2015年7月31日 下午6:00:26 
  * @version V1.0   
  */
-package org.begincode.core.contant;
+package org.begincode.core.constant;
 
 /**
- * @ClassName: Contants
+ * @ClassName: BeginCodeConstant
  * @Description: begincode静态常量
  * @author liutao
  * @date 2015年7月31日 下午6:00:26
  * 
  */
-public final class Contants {
+public final class BeginCodeConstant {
 
 	/**
 	 * 删除标识 delete_flag, 1：可用 0：删除
@@ -35,6 +35,12 @@ public final class Contants {
 	 * */
 	public static final int NAV_VIDEO_COURSE = 1;
 	public static final int NAV_CODE_SHARE = 2;
+	
+	/**
+	 * 审核标识
+	 * */
+	public static final String CHECK_PAAS = "1";
+	public static final String CHECK_WAIT = "0";
 	
 
 }
